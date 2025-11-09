d3cf91f3dde0aea6919bec74d06ce728bd5e9abc
sora81dev <sora81dev@gmail.com>
2025-11-09 17:08:44 +0900

    Fix: post date

--- scripts/discord.js
+++ scripts/discord.js
@@ -50,7 +50,7 @@ async function main() {
   await new Promise((resolve) => client.once(Events.ClientReady, resolve));
   console.log(`✅ Logged in as ${client.user.tag}`);
 
-  cron.schedule("17 18 * * 0-5", () => {
+  cron.schedule("16 15 * * 0-5", () => {
     sendTextMessage(tomorrow);
   });
 }
